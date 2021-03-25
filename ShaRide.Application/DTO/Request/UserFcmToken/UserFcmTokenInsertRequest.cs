bbcc7e86665a35fba097fe567a2b1f095b7c4e7d
mustafayevt <mustafayevtural46@gmail.com>
2021-03-25 19:12:18 +0400

    Token in UserFcmTokenInsertRequest.cs made required for avoiding null token inserting

--- ShaRide.Application/DTO/Request/UserFcmToken/UserFcmTokenInsertRequest.cs
+++ ShaRide.Application/DTO/Request/UserFcmToken/UserFcmTokenInsertRequest.cs
@@ -1,8 +1,13 @@
-﻿namespace ShaRide.Application.DTO.Request.UserFcmToken
+﻿using System.ComponentModel.DataAnnotations;
+using ShaRide.Application.Helpers;
+
+namespace ShaRide.Application.DTO.Request.UserFcmToken
 {
     public class UserFcmTokenInsertRequest
     {
         public int UserId { get; set; }
+        
+        [Required(ErrorMessage = LocalizationKeys.REQUIRED)]
         public string Token { get; set; }
     }
 }
